ffdcd28ee46e912b1be91ede296b85d0e7cfd755
Shkyrmetoffa <shkyrmetoffa@gmail.com>
2018-03-05 17:41:34 +0200

    connect added to LibraryList

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; 
+import reducers from './reducers'; 
+import { Header } from './components/common/Header';
 
 const App = () => {
     return (
-        <View />
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText='Tech stack' />
+        </View>
+      </Provider>
     );
 };
 
